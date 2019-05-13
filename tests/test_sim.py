cc2c66ece6a110948440df0e5ab1f2050af13d9c
Tobias Richter <tr4711@gmail.com>
2019-05-13 12:39:12 +0200

    Split test files

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -4,22 +4,11 @@
 import math
 import unittest
 
-# 3rd party modules
-import gym
-
 # internal modules
-import gym_banana
 from gym_banana.simulation import AlreadySoldError, LeadtimePassedError, IntradayMarket
 
 
 class Environments(unittest.TestCase):
-    # def test_env(self):
-    #     env = gym.make("Banana-v0")
-    #     env.seed(0)
-    #     env.reset()
-    #     env.step(0)
-    #     return False
-
     def test_probability(self):
         market = IntradayMarket()
 
