20d01d517461fc75c9af7d5eb2756b5cafcf93e4
Tobias Richter <tr4711@gmail.com>
2019-05-13 23:06:13 +0200

    Add tensorflow depenency

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ from setuptools import setup
 setup(
     name="marketsim",
     version="0.0.1",
-    install_requires=["keras-rl>=0.4.2", "gym>=0.2.3", "pandas>=0.23.4", "cfg_load"],
+    install_requires=[
+        "keras-rl>=0.4.2",
+        "gym>=0.2.3",
+        "pandas>=0.23.4",
+        "tensorflow>=1.13.0",
+        "cfg_load",
+    ],
 )
