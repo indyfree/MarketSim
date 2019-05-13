e7c7e7a768c813cd135274890a5897f16a8ac775
Tobias Richter <tr4711@gmail.com>
2019-05-13 11:53:05 +0200

    Test simulation

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,6 +9,8 @@ import gym
 
 # internal modules
 import gym_banana
+from gym_banana import simulation
+from gym_banana.simulation.market import selling_chance
 
 
 class Environments(unittest.TestCase):
@@ -22,6 +24,27 @@ class Environments(unittest.TestCase):
 
         price = math.inf
         self.assertTrue(0 == selling_chance(price))
+
+    def test_sim(self):
+        market = simulation.IntradayMarket(slots_ahead=2, max_price=2)
+
+        reward = market.trade_offer(0)
+        self.assertTrue(0 == reward)
+
+        self.assertRaises(RuntimeError, market.trade_offer, 0)
+
+        market.reset()
+        reward = market.trade_offer(0)
+        self.assertTrue(0 == reward)
+
+        market.reset()
+        reward = market.trade_offer(math.inf)
+        self.assertTrue(0 == reward)
+
+        reward = market.trade_offer(math.inf)
+        self.assertTrue(0 == reward)
+
+        self.assertRaises(RuntimeError, market.trade_offer, math.inf)
 
 
 if __name__ == "__main__":
