8488a7d983625a291917ff69885895b4f3651ea3
Tobias Richter <tr4711@gmail.com>
2019-05-13 16:05:29 +0200

    Test with random agent

--- tests/test_env.py
+++ tests/test_env.py
@@ -14,10 +14,32 @@ import gym_banana
 class Environments(unittest.TestCase):
     def test_env(self):
         env = gym.make("Banana-v0")
-        env.seed(0)
+        env.seed(123)
+        env.observation_space.sample()
+        env.action_space.sample()
         env.reset()
         env.step(0)
-        return False
+
+    def test_random_agent(self):
+        env = gym.make("Banana-v0")
+        env.seed(123)
+        ob = env.reset()
+        print("Init: {} steps left".format(ob[0]))
+
+        cum_reward = 0
+        for i in range(10):
+            print("--- Timestep: {} ----".format(i))
+            a = env.action_space.sample()
+            ob, reward, done, _ = env.step(a)
+            cum_reward += reward
+            print("Reward: {:.2f} EUR".format(reward))
+            print("Observation: {} steps left".format(ob[0]))
+            if done:
+                ob = env.reset()
+                print("Reset: {} steps left".format(ob[0]))
+
+        print("Mean reward {:.2f} EUR".format(cum_reward / 5))
+        print("Total reward {:.2f} EUR".format(cum_reward))
 
 
 if __name__ == "__main__":
